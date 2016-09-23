ea4c612fe098d6ecd8cf4bbe5d1ffb56b4663aa7
Thomas Roux <thom.roux@gmail.com>
2016-09-23 23:10:37 +0200

    debug

--- public/javascripts/app/app.component.ts
+++ public/javascripts/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component, Pipe, PipeTransform, NgZone} from 'angular2/core';
 
+import { Observable }     from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+
 @Pipe({name: 'keys'})
 export class KeysPipe implements PipeTransform {
   transform(value, args:string[]) : any {
@@ -21,14 +24,16 @@ export class AppComponent {
     socket = null;
 
     //room: Object = { sensor : {} };// = room;
-    config = {};
+    //config = {};
     //date = new Date();
+
+    _config : Subject<any>
 
     constructor(private zone:NgZone){
         this.socket = io('http://pictrl1.local:8000');
         this.socket.on('init', function(data){
-            this.config = data;
-            console.log(this.config);
+            //this.config = data;
+            this._config.next(data);
         }.bind(this));
         /*this.socket.on('roomInit', function(data){
           this.room = data;
@@ -39,6 +44,10 @@ export class AppComponent {
         this.socket.on('valueChanged', function(data){
           this.room.switches[data.id].value = data.value;
         }.bind(this));*/
+    }
+
+    get config() {
+      return this._config.asObservable();
     }
 
     /*changeValue(id, value) {
