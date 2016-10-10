170355b0be5149a7832bb7fa20cc4371e564d3b7
Thomas Roux <thom.roux@gmail.com>
2016-10-10 17:15:54 +0200

    test

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ var LED = function(_name, _outputPin, _switchPin, _rpio) {
   this.dimmer = function(value) {
     //if (value==0) _rpio.write(this.outputPin, _rpio.HIGH);
     //else _rpio.write(this.outputPin, _rpio.LOW);
-    _rpio.pwmSetData(this.outputPin, value+0);
+    _rpio.pwmSetData(this.outputPin, Math.round(value*10.24));
     this.level = value;
     this.on = (value>0);
     io.emit('valueChanged',this);
