08e6850c30d4b7012ced1d7acddb9810b56c1789
Thomas Roux <thom.roux@gmail.com>
2016-10-10 18:10:47 +0200

    adjusted numbers

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ var LED = function(_name, _outputPin, _switchPin, _rpio) {
 
   //_rpio.open(this.outputPin, _rpio.OUTPUT, _rpio.LOW);
   _rpio.open(this.outputPin, _rpio.PWM);
-  _rpio.pwmSetClockDivider(1024);
+  _rpio.pwmSetClockDivider(32);
   _rpio.pwmSetRange(this.outputPin, 1024);
   _rpio.pwmSetData(this.outputPin, 0);
   _rpio.open(this.switchPin, _rpio.INPUT, _rpio.PULL_UP);
